7f95813e5cb774e068d5784f1561a9fbef8168b0
Konrad Hendley <konradhendley@Konrads-MacBook-Pro.local>
2025-02-22 19:44:28 -0700

    updates to the config file

---
+++ frontend/src/config.js
@@ -0,0 +1,5 @@
+export const config = {
+    USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID, 
+    CLIENT_ID: process.env.REACT_APP_CLIENT_ID,  
+    REGION: process.env.REACT_APP_REGION,     
+};
