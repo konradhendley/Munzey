b85d6f2da11c1192420e8ced42a12aab1d971cf9
Konrad Hendley <konradhendley8@gmail.com>
2026-02-16 16:42:47 -0700

    readme updates

--- frontend/src/pages/home.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from './Home';
-
-test('renders welcome text', () => {
-  render(<Home />); // Render the Home component
-  const welcomeText = screen.getByText(/Welcome to Folio/i); 
-  expect(welcomeText).toBeInTheDocument();
-});
